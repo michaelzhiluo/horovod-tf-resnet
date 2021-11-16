1bc21f3da347e364e182b2e474e8a5a4a83bd98e
Michael Luo <michaelluo@MacBook-Pro.local>
2021-11-16 13:08:31 -0800

    Fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # NCCL 2
+cd ~/
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo dpkg -i ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
@@ -8,22 +9,19 @@ echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu' >> ~/.b
 source ~/.bashrc
 
 # OpenMPI
-sudo mv /usr/bin/mpirun /usr/bin/bk_mpirun
-sudo mv /usr/bin/mpirun.openmpi /usr/bin/bk_mpirun.openmpi
-sudo mv /usr/bin/orted /usr/bin/bk_orted
-sudo mv /opt/amazon/openmpi/ /opt/amazon/b_openmpi/
+cd ~/
 wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.gz
 tar -xvf openmpi-4.0.1.tar.gz
 cd ./openmpi-4.0.1
 ./configure --prefix=$HOME/openmpi
 sudo make -j 8 all
 sudo make install
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/openmpi/lib' >> ~/.bashrc
-echo 'export PATH=$PATH:~/openmpi/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=~/openmpi/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PATH=~/openmpi/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
 # Setup Python Environment
-sudo apt install g++-4.8
+sudo apt --assume-yes install g++-4.8
 echo alias python=python3 >> ~/.bashrc
 echo alias pip=pip3 >> ~/.bashrc
 source ~/.bashrc
