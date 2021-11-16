cc22279da553af43e0bd9f6ff4b586d4ae7b2aff
Michael Luo <michaelluo@MacBook-Pro.local>
2021-11-16 13:54:15 -0800

    Pip install for python3

--- setup.sh
+++ setup.sh
@@ -25,5 +25,5 @@ sudo apt --assume-yes install g++-4.8
 echo alias python=python3 >> ~/.bashrc
 echo alias pip=pip3 >> ~/.bashrc
 source ~/.bashrc
-pip install tensorflow-gpu==1.14.0 keras==2.3.0
-HOROVOD_NCCL_HOME=/usr/lib/x86_64-linux-gnu HOROVOD_GPU_ALLREDUCE=NCCL HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITHOUT_PYTORCH=1 HOROVOD_WITHOUT_MXNET=1 pip install --no-cache-dir horovod==0.18.0
+pip3 install tensorflow-gpu==1.14.0 keras==2.3.0
+HOROVOD_NCCL_HOME=/usr/lib/x86_64-linux-gnu HOROVOD_GPU_ALLREDUCE=NCCL HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITHOUT_PYTORCH=1 HOROVOD_WITHOUT_MXNET=1 pip3 install --no-cache-dir horovod==0.18.0
