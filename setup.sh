f29ca7654cacdf097acb4047df678eae4cd98d73
Michael Luo <michael.luo@berkeley.edu>
2021-11-16 20:45:05 -0800

    Fix

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ cd ~/
 sudo mv /usr/bin/mpirun /usr/bin/bk_mpirun
 sudo mv /usr/bin/mpirun.openmpi /usr/bin/bk_mpirun.openmpi
 sudo mv /usr/bin/orted /usr/bin/bk_orted
-sudo mv /opt/amazon/openmpi/ /opt/amazon/b_openmpi/
+sudo mv /opt/amazon/openmpi/ /opt/amazon/bk_openmpi/
 wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.gz
 tar -xvf openmpi-4.0.1.tar.gz
 cd ./openmpi-4.0.1
@@ -32,5 +32,5 @@ echo alias pip=pip3 >> ~/.bashrc
 source ~/.bashrc
 pip3 install tensorflow-gpu==1.14.0 keras==2.3.0
 HOROVOD_NCCL_HOME=/usr/lib/x86_64-linux-gnu HOROVOD_GPU_ALLREDUCE=NCCL HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITHOUT_PYTORCH=1 HOROVOD_WITHOUT_MXNET=1 pip3 install --no-cache-dir horovod==0.18.0
-pip3 uninstall horovod
+pip3 uninstall -y horovod
 HOROVOD_NCCL_HOME=/usr/lib/x86_64-linux-gnu HOROVOD_GPU_ALLREDUCE=NCCL HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITHOUT_PYTORCH=1 HOROVOD_WITHOUT_MXNET=1 pip3 install --no-cache-dir horovod==0.18.0
